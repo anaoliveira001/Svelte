8bc8184b5fbc690a7c0c751f7be28b46100d6ebf
Ana Paula <ana.oliveira.prt_a@msft.cesae.pt>
2022-10-15 16:58:14 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	
+</script>
+
+	<div class="app">
+		<header>
+			<h1 class="titulo"> Svelte Perfis - Github</h1>
+		</header>
+
+	</div>
+<style>
+	    .app {
+    max-height: 100vh;
+  }
+
+  header {    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .titulo {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 800;
+    font-size: 38px;
+    line-height: 45px;
+
+    color: #3680f4;
+    background-image: linear-gradient(to left, #3680f4, #c14fac);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+</style>
