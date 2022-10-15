8d656bde0d11498f7ec2a74b4478f3a4c57374bf
Ana Paula <ana.oliveira.prt_a@msft.cesae.pt>
2022-10-15 17:05:15 +0100

    Update - Titulo

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+  import Titulo from "./components/Titulo.svelte";
+
 	
 </script>
-
 	<div class="app">
 		<header>
-			<h1 class="titulo"> Svelte Perfis - Github</h1>
+			<Titulo />
 		</header>
 
 	</div>
+
 <style>
 	    .app {
     max-height: 100vh;
@@ -18,19 +20,4 @@
     align-items: center;
     justify-content: space-between;
   }
-
-  .titulo {
-    font-family: "Roboto";
-    font-style: normal;
-    font-weight: 800;
-    font-size: 38px;
-    line-height: 45px;
-
-    color: #3680f4;
-    background-image: linear-gradient(to left, #3680f4, #c14fac);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-
 </style>
