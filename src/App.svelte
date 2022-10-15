f4b7eb8bb1aca58b1f8c2dfde97f8379206e9ed7
Ana Paula <ana.oliveira.prt_a@msft.cesae.pt>
2022-10-15 19:02:29 +0100

    Update - Props

--- src/App.svelte
+++ src/App.svelte
@@ -1,82 +1,49 @@
 <script lang="ts">
-
-  import BarraSuperior from "./components/BarraSuperior.svelte";
   import Titulo from "./components/Titulo.svelte";
+  import Usuario from "./components/Usuario.svelte";
   import type IUsuario from "./interfaces/IUsuario";
-  
 
-  let usuario: IUsuario = {
-    login: "anaoliveira001",
-    avatar_url: "https://github.com/anaoliveira001.png",
-    perfil_url: "https://github.com/anaoliveira001",
-    nome: "Ana Paula Oliveira",
-    repositorios_publicos: 20,
-    seguidores: 10
-  };
+  let usuario: IUsuario | null = null;
 
-  let valorImput = 'Svelte';
+  let valorImput = "Svelte";
 
-  function aoSubmeter(){
-	console.log(valorImput);
+  function aoSubmeter() {
+    usuario = {
+      login: "anaoliveira001",
+      avatar_url: "https://github.com/anaoliveira001.png",
+      perfil_url: "https://github.com/anaoliveira001",
+      nome: "Ana Paula Oliveira",
+      repositorios_publicos: 20,
+      seguidores: 10,
+    };
   }
 </script>
 
-	<div class="app">
-		<header>
-			<Titulo />
-			<div class="busca-usuario">
-				<form on:submit|preventDefault={aoSubmeter}>
-					<input type="text" class="input" bind:value={valorImput}>
+<div class="app">
+  <header>
+    <Titulo />
+    <div class="busca-usuario">
+      <form on:submit|preventDefault={aoSubmeter}>
+        <input type="text" class="input" bind:value={valorImput} />
 
-					<div class="button-container">
-						<button type="submit" class="button">Search</button>
-					</div>
-				</form>
-			</div>
-		</header>
+        <div class="button-container">
+          <button type="submit" class="button">Search</button>
+        </div>
+      </form>
+    </div>
+  </header>
 
-	<div class="card-usuario">
-			<BarraSuperior />
-
-			<div class="usuario">
-				<div class="foto-container">
-    			<a href={usuario.perfil_url} target="_blanck" rel="noopener">
-        	<div 
-				class="foto-usuario"
-				style:background-image="url({usuario.avatar_url})"
-			></div></a>
-			
-    			
-	</div>
-
-			<div class="detalhes-usuario">
-				<div class="info">
-					Name: <span>{usuario.nome}</span>
-				</div>
-
-				<div class="info">
-					User: <span>{usuario.login}</span>
-				</div>
-
-				<div class="info">
-					Followers: <span>{usuario.seguidores}</span>
-				</div>
-
-				<div class="info">
-					Repositories: <span>{usuario.repositorios_publicos}</span>
-				</div>
-
-			</div>
-			</div>
-		</div>
-	</div>
+  {#if usuario}
+    <Usuario {usuario} />
+  {/if}
+</div>
 
 <style>
-	.app {
+  .app {
     max-height: 100vh;
   }
 
-  header {    
+  header {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -134,48 +101,7 @@
 
   .button:hover {
     background: #000000;
-  } 
-
-  .card-usuario {
-    margin-top: 65px;
   }
 
-  .usuario {
-    padding: 28px 0;
-    background: rgba(255, 255, 255, 0.5);
-    box-shadow: -12px 37px 45px rgba(133, 127, 201, 0.18);
-    border-radius: 0px 0px 13px 13px;
-
-    display: flex;
-    justify-content: center;
-  }
-
-  .foto-container {
-    margin-right: 81px;
-  }
-
-  .foto-usuario {
-    width: 12.75rem;
-    height: 12.75rem;
-    border: 4.56px solid #be15a5;
-    border-radius: 50%;
-    background-size: cover;
-  }
-
-  .detalhes-usuario {
-    margin-right: 55px;
-  }
-
-  .detalhes-usuario > .info {
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 31px;
-    color: #395278;
-  }
-
-  .detalhes-usuario > .info > span {
-    color: #6781a8;
-    font-weight: normal;
-  }
-
+  
 </style>
